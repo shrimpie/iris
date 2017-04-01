00e0efcb5f6f40865492eb5f1a742280b34a2ad2
杨振宇 <andy@yang.local>
2017-03-31 21:39:20 -0400

    refactor witClient

--- server/witClient.js
+++ server/witClient.js
@@ -6,11 +6,16 @@ function handleWitResponse(res) {
     return res.entities;
 }
 
-module.exports = function witClient(token) {
-    const ask = function ask(message, cb) {
+class WitClient {
+
+    constructor(token) {
+        this._token = token;
+    }
+
+    ask(message, cb) {
 
         request.get('https://api.wit.ai/message')
-            .set('Authorization', 'Bearer ' + token)
+            .set('Authorization', 'Bearer ' + this._token)
             .query({v: '20160919'})
             .query({q: message})
             .end((err, res) => {
@@ -21,10 +26,13 @@ module.exports = function witClient(token) {
                 const witResponse = handleWitResponse(res.body);
                 return cb(null, witResponse);
             })
-
     }
 
-    return {
-        ask: ask
-    }
+
 }
+
+module.exports = WitClient;
+
+
+
+
